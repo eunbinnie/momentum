4fde702f42ccede39a65c1ba85f015b91c15b011
eunbinnie <dmsqlsdl54@naver.com>
2023-01-05 21:54:06 +0900

    jsVariables

---
+++ app.js
@@ -0,0 +1,14 @@
+// variable 변수
+const a = 5;
+const b = 2;
+
+console.log(a + b);
+console.log(a * b);
+console.log(a / b);
+
+let myName = 'eunbin';
+console.log('hello ' + myName); // hello eunbin
+
+myName = 'eunbinlee';
+console.log('your new name is ' + myName);  // your new name is eunbinlee
+console.log('---------------------------');
