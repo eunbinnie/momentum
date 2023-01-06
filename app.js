91821f7a60f225851f4b0943780e2f9f490b88a7
eunbinnie <dmsqlsdl54@naver.com>
2023-01-06 12:09:17 +0900

    make array, search, push

--- app.js
+++ app.js
@@ -12,3 +12,16 @@ console.log('hello ' + myName); // hello eunbin
 myName = 'eunbinlee';
 console.log('your new name is ' + myName);  // your new name is eunbinlee
 console.log('---------------------------');
+
+
+// array(배열)
+const daysOfWeek = ["mon", "tue", "wed", "thu", "fri", "sat"];
+console.log(daysOfWeek);
+
+// Get Item from Array
+console.log(daysOfWeek[4]);
+
+// Add one more day to the array
+daysOfWeek.push("sun");
+
+console.log(daysOfWeek);
