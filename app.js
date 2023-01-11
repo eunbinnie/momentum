4ec64b90895083ff5b8207379daeb9096f014427
eunbinnie <dmsqlsdl54@naver.com>
2023-01-11 20:51:46 +0900

    more events

--- app.js
+++ app.js
@@ -202,13 +202,44 @@
 // event listener
 {
 
-  const title = document.querySelector("#title");
+  const h1 = document.querySelector("#title");
 
   function handleTitleClick() {
-    title.style.color = "red";
+    h1.style.color = "red";
     console.log("title was clicked!");
   }
 
-  title.addEventListener("click", handleTitleClick);
+  function handleMouseEnter() {
+    h1.innerText = "Mouse is here!";
+  }
+
+  function handleMouseLeave() {
+    h1.innerText = "Mouse is gone!";
+  }
+
+  function handleWindowResize() {
+    document.body.style.backgroundColor = "tomato";
+  }
+
+  function handleWindowCopy() {
+    alert("copier!");
+  }
+
+  function handleWindowOffline() {
+    alert("SOS no WIFI");
+  }
+
+  function handleWindowOnline() {
+    alert("ALL GOOD");
+  }
+
+  h1.addEventListener('click', handleTitleClick);
+  h1.addEventListener('mouseenter', handleMouseEnter);
+  h1.addEventListener('mouseleave', handleMouseLeave);
+
+  window.addEventListener('resize', handleWindowResize);
+  window.addEventListener('copy', handleWindowCopy);
+  window.addEventListener('offline', handleWindowOffline);
+  window.addEventListener('online', handleWindowOnline);
 
 }
