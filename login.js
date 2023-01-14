99196832acd7c5d1269c83ce84e0b34f38370eb2
eunbinnie <dmsqlsdl54@naver.com>
2023-01-14 18:43:27 +0900

    getting username

--- login.js
+++ login.js
@@ -1,29 +1,15 @@
 const loginForm = document.querySelector("#login-form");
 const loginInput = document.querySelector("#login-form input");
+const greeting = document.querySelector("#greeting");
 
-const link = document.querySelector("a");
-
-// 내가 작성한 코드
-// function onLoginBtnClick() {
-//   if (loginInput.value === "") {
-//     console.log("Text your name.");
-//   } else if (loginInput.value.length > 20) {
-//     console.log("Your name is too long.");
-//   } else {
-//     console.log("Hello " + loginInput.value);
-//   }
-// }
+const HIDDEN_CLASSNAME = "hidden";
 
 function onLoginSubmit(event) {
   event.preventDefault();
-  // const username = loginInput.value;
-  console.log(loginInput.value);
-}
-
-function handleLinkClick(event) {
-  event.preventDefault();
+  loginForm.classList.add(HIDDEN_CLASSNAME);
+  const username = loginInput.value;
+  greeting.innerText = `Hello ${username}`
+  greeting.classList.remove(HIDDEN_CLASSNAME);
 }
 
 loginForm.addEventListener('submit', onLoginSubmit);
-
-link.addEventListener('click', handleLinkClick);
