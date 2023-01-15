be4d1c3cfb4aae5c9c3fafdb4b6de5e7b02c18f7
eunbinnie <dmsqlsdl54@naver.com>
2023-01-15 17:50:28 +0900

    setTimeout(), new Date()

--- js/clock.js
+++ js/clock.js
@@ -1,8 +1,14 @@
 const clock = document.querySelector("#clock");
 
-function sayHello() {
-  console.log("Hello");
+function getClock() {
+  const date = new Date();
+  clock.innerText = `${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`;
 }
 
+// site load 되자마자 시간 출력 위해서
+getClock();
 // setInterval(function name, time)
-setInterval(sayHello, 5000);
+setInterval(getClock, 1000);
+
+// setTimeout(function, time)
+// setTimeout(getClock, 5000);
