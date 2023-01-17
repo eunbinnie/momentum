d130e5d331086fbee38b4d6f403554a67140afb7
eunbinnie <dmsqlsdl54@naver.com>
2023-01-17 12:46:46 +0900

    ul 안에 todolist 저장

--- js/todo.js
+++ js/todo.js
@@ -2,11 +2,21 @@ const toDoForm = document.getElementById("todo-form");
 const toDoInput = toDoForm.querySelector("input");
 const toDoList = document.getElementById("todo-list");
 
+// add li at html
+function paintToDo(todo) {
+  const li = document.createElement("li");
+  const span = document.createElement("span");
+  li.appendChild(span);
+  span.innerText = todo;
+  toDoList.appendChild(li);
+}
+
+// after form enter
 function handleToDoSubmit(event) {
   event.preventDefault();
   const newTodo = toDoInput.value;
   toDoInput.value = "";
-  console.log(newTodo, toDoInput.value);
+  paintToDo(newTodo);
 }
 
 toDoForm.addEventListener('submit', handleToDoSubmit);
